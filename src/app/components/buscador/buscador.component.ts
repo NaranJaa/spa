6480f086ffaf5ce3b5ca97ee0a6b26561cea9e24
Genoveva <gflores@queplan.cl>
2019-12-03 09:32:22 -0300

    Se crea nuevo componente para reutilizar codigo de la tarjeta

--- src/app/components/buscador/buscador.component.ts
+++ src/app/components/buscador/buscador.component.ts
@@ -12,13 +12,13 @@ export class BuscadorComponent implements OnInit {
   heroes:any[] = [];
   termino:string;
   constructor(private activatedRoute:ActivatedRoute,
-              private _heroesService:HeroesService) { }
+              private _heroesService: HeroesService) { }
 
   ngOnInit() {
-
     this.activatedRoute.params.subscribe(params => {
       this.termino = params['termino'];
       this.heroes = this._heroesService.buscarHeroes(params['termino']);
-    })
+    });
   }
+
 }
